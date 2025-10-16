690c4b0eddab2341a64ab9d7e7e7afe87b801fd2
Ajay Singh Rathore <ajayrathore990@gmail.com>
2025-10-16 18:44:34 +0530

    Added solution 2 for leetcode184

--- medium/department_highest_salary.sql
+++ medium/department_highest_salary.sql
@@ -4,6 +4,9 @@ For the above tables, your SQL query should return the following rows
 (order of rows does not matter).
 */
 
+--Solution 1
+
+--Hash Join  (cost=116.78..144.00 rows=31 width=72)
 with cte as (
 select
 	*
@@ -27,3 +30,41 @@ where
 	c.rank = 1;
 
 
+
+--Solution 2
+
+--CTE Scan on cte2  (cost=366.19..510.10 rows=6364 width=72)
+with cte1 as (
+select
+	max(salary) over(partition by departmentid) as max_salary,
+	*
+from
+	leetcode184.employees
+),
+cte2 as 
+(
+select
+	e.id as e_id ,
+	e.name as e_name,
+	e.salary,
+	e.departmentid,
+	d.id as d_id,
+	d.name as d_name,
+	case
+		when max_salary = salary then e.name
+		else 'XXX'
+	end as sol
+from
+	cte1 as e
+join leetcode184.Department as d 
+on
+	e.departmentid = d.id 
+)
+select
+	e_name,
+	d_name,
+	salary
+from
+	cte2
+where
+	sol <> 'XXX';
